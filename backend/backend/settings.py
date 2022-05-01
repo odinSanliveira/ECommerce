28582a073d9201aefd5517a2727521bdb5f4d0b2
odinSanliveira <odin.sanliveira@gmail.com>
2022-05-01 12:25:53 -0300

    adding libraries to settings, configuring admin and creating basic serializers.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT='static/images'
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
