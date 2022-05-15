e5fac5518dfbe13c9cdadc2ca65de7591091a256
odinSanliveira <odin.sanliveira@gmail.com>
2022-05-15 11:39:49 -0300

    authentication for user, routes configuration, user tokens response, views to user profile and access control.

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,10 +1,42 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from .models import *
 
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'isAdmin']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            obj.email
+        return name
+
+    def get_isAdmin(self, obj):
+        admin = obj.is_staff
+        return admin
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProductsId, name="product"),
+    path('user/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/register', views.registerUser, name='user_register'),
+    path('user/profile', views.getUserProfile, name='user_profile'),
+    path('users/', views.getUsers, name='users_profiles'),
+
 
 ]
 
