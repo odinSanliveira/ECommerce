2a730093a7ff8899f6fdf565abb61d41a7a224e4
odinSanliveira <odin.sanliveira@gmail.com>
2022-05-16 22:16:49 -0300

    creating login screen.

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
+import { login } from '../actions/userActions'
+import { useNavigate, useLocation } from 'react-router-dom';
+
+function LoginScreen() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const history = useNavigate();
+    const { search } = useLocation();
+
+    const dispatch = useDispatch()
+    const redirect = search.pathname ? search.split('=')[1] : '/'
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { error, loading, userInfo } = userLogin
+
+    useEffect(() => {
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+        console.log('url: ', redirect)
+    }
+
+    return (
+        <FormContainer>
+            <h1>Sign In</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+
+                <Form.Group controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Enter Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+                <Row className='py-4'>
+                    
+                    <Button type='submit' variant='primary'>
+                        Sign In
+                    </Button>
+                    
+                </Row>
+                
+            </Form>
+
+            <Row className='py-4'>
+                <Col>
+                    New Customer? <Link
+                        to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+                        Register
+                        </Link>
+                </Col>
+            </Row>
+
+        </FormContainer>
+    )
+}
+
+export default LoginScreen
