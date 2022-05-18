8fae8ac7fbd45e59b7640fead718b023d817c04f
odinSanliveira <odin.sanliveira@gmail.com>
2022-05-17 21:55:34 -0300

    backend bug fixes, and rename urls endpoint.

--- backend/base/urls/users_urls.py
+++ backend/base/urls/users_urls.py
@@ -4,8 +4,8 @@ from base.views import users_views as views
 
 urlpatterns = [
     path('', views.getUsers, name='users_profiles'),
-    path('login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('register', views.registerUser, name='user_register'),
-    path('profile', views.getUserProfile, name='user_profile'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', views.registerUser, name='user_register'),
+    path('profile/', views.getUserProfile, name='user_profile'),
 ]
 
