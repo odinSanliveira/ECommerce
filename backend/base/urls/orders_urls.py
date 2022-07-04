e77c43a88f8273dadbfe6c11be066ea731240ff6
odinSanliveira <odin.sanliveira@gmail.com>
2022-07-03 22:50:42 -0300

    updating orders_url.py

--- backend/base/urls/orders_urls.py
+++ backend/base/urls/orders_urls.py
@@ -4,5 +4,9 @@ from base.views import orders_views as views
 
 urlpatterns = [
     path('add/', views.addOrdersItems, name='orders-add'),
+     path('myorders/', views.getMyOrders, name='myorders'),
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-delivered'),
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
 ]
 
