5d19d2a9344898003ba9d7ee884b88a276483b53
odinSanliveira <odin.sanliveira@gmail.com>
2022-07-07 19:38:25 -0300

    fixing product view and orders urls configuration.

--- backend/base/urls/orders_urls.py
+++ backend/base/urls/orders_urls.py
@@ -5,9 +5,7 @@ from base.views import orders_views as views
 urlpatterns = [
     path('', views.getOrders, name='orders'),
     path('add/', views.addOrdersItems, name='orders-add'),
-    path('myorders/', views.getMyOrders, name='myorders'),
-    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-delivered'),
-    path('myorders/', views.getMyOrders, name='myorders'),
+    path('myorders/', views.getMyOrders, name='my-orders'),
     path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-delivered'),
     path('<str:pk>/', views.getOrderById, name='user-order'),
 
