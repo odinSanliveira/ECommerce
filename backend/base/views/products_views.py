b66e8264a739f2ef8a66a7629446895d1c7cc3cb
odinSanliveira <odin.sanliveira@gmail.com>
2022-07-11 23:28:47 -0300

    cleaning products_views.py

--- backend/base/views/products_views.py
+++ backend/base/views/products_views.py
@@ -19,8 +19,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @api_view(['GET'])
 def getProducts(request):
-    query = request.query_params.get('keyword')    
-    print('query: ', query)
+    query = request.query_params.get('keyword')
     if query == None:
         query = ''
     products = Product.objects.filter(name__contains=query)
@@ -40,8 +39,8 @@ def getProductsId(request, pk):
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
 
-@api_view(['POST'])
 
+@api_view(['POST'])
 @permission_classes([IsAdminUser])
 def createProduct(request):
     user = request.user
@@ -59,24 +58,6 @@ def createProduct(request):
 
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
-
-# @api_view(['GET'])
-# @permission_classes([IsAdminUser])
-# def getProductsId(request, pk):
-#     data = request.data
-#     product = Product.objects.get(_id=pk)
-
-#     product.name = data['name']
-#     product.price = data['price']
-#     product.brand = data['brand']
-#     product.countInStock = data['countInStock']
-#     product.category = data['category']
-#     product.description = data['description']
-
-#     product.save()
-
-#     serializer = ProductSerializer(product, many=False)
-#     return Response(serializer.data)
 
 @api_view(['PUT'])
 @permission_classes([IsAdminUser])
@@ -105,7 +86,7 @@ def deleteProduct(request, pk):
 
 @api_view(['POST'])
 def uploadImage(request):
-    fixed_height = 320
+    # fixed_height = 320
     data = request.data
 
     product_id = data['product_id']
@@ -117,7 +98,6 @@ def uploadImage(request):
     #product.image = product.image.resize((width_size, fixed_height), PIL.Image.NEAREST)
 
     product.save()
-
     return Response('Image was uploaded')
 
 @permission_classes([IsAuthenticated])
